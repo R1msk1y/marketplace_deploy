886477db11fb3f9546c5f6b4df914dfd67965947
Kurganov Vasiliy <wkurganow@gmail.com>
2023-08-17 16:08:08 +0300

    17/08/23
    правки

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -1,7 +1,12 @@
 import axios from "axios";
 import styles from "./feedback.module.scss";
 import { useForm } from "react-hook-form";
+import React, { useState } from "react";
 const Feedback = () => {
+  const [stateForm, setStateForm] = useState(false);
+  const checkForm = () => {
+    setStateForm(!stateForm);
+  };
   const {
     register,
     formState: { errors },
@@ -14,79 +19,86 @@ const Feedback = () => {
     axios.post("https://64dbcbc0593f57e435b16da2.mockapi.io/feedback", data);
     console.log(JSON.stringify(data));
     reset();
+    setStateForm(!stateForm);
   };
   return (
     <div>
       <h2 className={styles.feedBack_title}>Обратная связь</h2>
-      <div style={{ display: "none" }} className={styles.feedback_comment}>
-        <h2>Ваш комментарий добавлен!</h2>
-        <h2>Спасибо за фидбэк!</h2>
-        <img src="img/feedback/comment_add.png" alt="CommentAdd" />
-      </div>
-      <form onSubmit={handleSubmit(onSubmit)} className={styles.feedBack_form}>
-        <div className={styles.inputContainer}>
-          <input
-            type="text"
-            placeholder="Имя"
-            {...register("name", {
-              required: "Поле обязательно к заполнению",
-              pattern: {
-                value: /^[А-Я][а-я]*/,
-                message: "С заглавной буквы / только кирилица",
-              },
-            })}
-          />
-          <div className={styles.inputCheck}>
-            {errors?.name && <p>{errors?.name?.message}</p>}
+      {stateForm === false ? (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className={styles.feedBack_form}
+        >
+          <div className={styles.inputContainer}>
+            <input
+              type="text"
+              placeholder="Имя"
+              {...register("name", {
+                required: "Поле обязательно к заполнению",
+                pattern: {
+                  value: /^[А-Я][а-я]*/,
+                  message: "С заглавной буквы / только кирилица",
+                },
+              })}
+            />
+            <div className={styles.inputCheck}>
+              {errors?.name && <p>{errors?.name?.message}</p>}
+            </div>
           </div>
-        </div>
 
-        <div className={styles.inputContainer}>
-          <input
-            type="tel"
-            placeholder="Телефон"
-            {...register("phone", {
-              required: "Поле обязательно к заполнению",
-              pattern: {
-                value: /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/,
-                message: `Введите телефон в формате "+79261234567"`,
-              },
-            })}
-          />
-          <div className={styles.inputCheck}>
-            {errors?.phone && <p>{errors?.phone?.message}</p>}
+          <div className={styles.inputContainer}>
+            <input
+              type="tel"
+              placeholder="Телефон"
+              {...register("phone", {
+                required: "Поле обязательно к заполнению",
+                pattern: {
+                  value: /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/,
+                  message: `Введите телефон в формате "+79261234567"`,
+                },
+              })}
+            />
+            <div className={styles.inputCheck}>
+              {errors?.phone && <p>{errors?.phone?.message}</p>}
+            </div>
           </div>
-        </div>
 
-        <div className={styles.inputContainer}>
-          <input
-            type="email"
-            placeholder="Почта"
-            {...register("email", {
-              required: "Поле обязательно к заполнению",
-              pattern: {
-                value:
-                  /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu,
-                message: "Введите корректный Email",
-              },
-            })}
-          />
-          <div className={styles.inputCheck}>
-            {errors?.email && <p>{errors?.email?.message}</p>}
+          <div className={styles.inputContainer}>
+            <input
+              type="email"
+              placeholder="Почта"
+              {...register("email", {
+                required: "Поле обязательно к заполнению",
+                pattern: {
+                  value:
+                    /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu,
+                  message: "Введите корректный Email",
+                },
+              })}
+            />
+            <div className={styles.inputCheck}>
+              {errors?.email && <p>{errors?.email?.message}</p>}
+            </div>
           </div>
-        </div>
 
-        <textarea
-          {...register("comment")}
-          placeholder="Комментарий"
-          className={styles.textArea}
-          cols="30"
-          rows="10"
-        ></textarea>
-        <button className={styles.btn} type="submit">
-          Отправить
-        </button>
-      </form>
+          <textarea
+            {...register("comment")}
+            placeholder="Комментарий"
+            className={styles.textArea}
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button className={styles.btn} type="submit">
+            Отправить
+          </button>
+        </form>
+      ) : (
+        <div className={styles.feedback_comment}>
+          <img src="img/feedback/comment_add.png" alt="CommentAdd" />
+          <h2>Ваш комментарий добавлен!</h2>
+          <h2>Спасибо за фидбэк!</h2>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,15 +2,22 @@ import styles from "./footer.module.scss";
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <img
-        src="https://static.insales-cdn.com/files/1/1172/14550164/original/markom_logo_bw.svg"
-        alt="logo"
-      />
+      <div className={styles.footerBox}>
+        <img src="img/icons/arrow_b.png" alt="arrow_b" />
+        <h2>Заслать письмо</h2>
+        <img src="img/icons/arrow_b.png" alt="arrow_b" />
+      </div>
+
       <div className={styles.contacts}>
-        <h2>Контакты</h2>
-        <strong>Почта: wkurganow@gmail.com</strong>
-        <strong>Телефон: +7 929 371 2550</strong>
-        <strong>Telegram: @wkurganow</strong>
+        <div className={styles.contactsBlock}>
+          <img src="img/icons/gmail.svg" alt="gmail" />
+          <strong>Почта: wkurganow@gmail.com</strong>
+        </div>
+
+        <div className={styles.contactsBlock}>
+          <img src="img/icons/telegram.svg" alt="gmail" />
+          <strong>Telegram: @wkurganow</strong>
+        </div>
       </div>
     </div>
   );
